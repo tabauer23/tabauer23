5d834dd01adf8e0c8a799d0017e6eb6962cbe8c2
Trevor Bauer <86111774+tabauer23@users.noreply.github.com>
2022-05-03 14:55:34 -0400

    source this code for connection

---
+++ Keyring Functions/keyring_script_topper.R
@@ -0,0 +1,17 @@
+library(keyring)
+library(DBI)
+#creates the keyring
+kb <- keyring::backend_file$new() 
+#unlocks the keyring
+service <- rstudioapi::showPrompt("Database service", "please enter service name")
+kb$keyring_unlock(keyring = paste0(Sys.info()[["nodename"]]), password = tolower(Sys.info()[["user"]]))
+
+con <- dbConnect(odbc::odbc(), dsn = service, 
+                 uid = tolower(Sys.info()[["user"]]), 
+                 pwd = keyring::backend_file$new()$get(service = paste0(tolower(Sys.info()[["user"]]), Sys.info()[["nodename"]], "EBIP"), 
+                                                       user = tolower(Sys.info()[["user"]]), 
+                                                       keyring = Sys.info()[["nodename"]]))
+
+kb$keyring_lock(Sys.info()[["nodename"]])
+
+# make this into a function, that the user types in the arg as the dsn they want.
